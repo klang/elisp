0e818169798cbf5c75966bbd92a50f81fd1e72b7
Karsten Lang <karstenlang@gmail.com>
2009-04-09 13:41:08 +0200

    renaming stuff

--- 98-test-driven.el
+++ 98-test-driven.el
@@ -1,84 +1,84 @@
-(defun ut-basename (file) "returns name.ext for file /path/name.ext"
+(defun bash-basename (file) "returns name.ext for file /path/name.ext"
   (first (last (split-string file "\\/"))))
 
-(defun ut-path (file) "returns /path/ for file /path/name.ext"
-  (first (split-string file (ut-basename file))))
+(defun bash-dirname (file) "returns /path/ for file /path/name.ext"
+  (first (split-string file (bash-basename file))))
 
-(defun ut-ext (file) "returns ext for file name.ext"
-  (first (last (split-string (ut-basename file) "\\."))))
-
-(defun ut-name (file) "returns name for file name.ext"
-  (car (split-string (ut-basename file) (concat "\\." (ut-ext file) "$") t)))
-
-(defun ut-trail-with-slash (path) "returns path/ for path or path/"
+(defun bash-dirname-trail-with-slash (path) "returns path/ for path or path/"
   (replace-regexp-in-string "/?$" "/" path) )
 
-(defun ut-test-file (file &optional marker) 
+(defun bash-extension (file) "returns ext for file name.ext"
+  (first (last (split-string (bash-basename file) "\\."))))
+
+(defun bash-filename (file) "returns name for file name.ext"
+  (car (split-string (bash-basename file) (concat "\\." (bash-extension file) "$") t)))
+
+(defun td-test-file (file &optional marker) 
   "returns a sensible test script filename, without path. 
 name.ext -> name-marker.ext if marker is given
          -> name-test.ext otherwise"
   (or marker (setq marker "-test"))
-  (concat (ut-name file) marker "." (ut-ext file)))
+  (concat (bash-filename file) marker "." (bash-extension file)))
 
 ;; (defun ut-doc-string (file) 
 ;;   "returns \"Unit testing 'name' using SimpleTest\""
-;;   (concat "Unit testing '" (ut-name file) "' using SimpleTest"))
+;;   (concat "Unit testing '" (bash-filename file) "' using SimpleTest"))
 
 ;; (defun ut-all (file) 
-;;    (let* ( (name     (concat "wrk-unittest-" (ut-name file))) 
+;;    (let* ( (name     (concat "wrk-unittest-" (bash-filename file))) 
 ;; 	   (docs     (ut-doc-string file))
-;; 	   (test     (concat (ut-path file) (ut-test-file file))) )
+;; 	   (test     (concat (bash-dirname file) (td-test-file file))) )
 ;;      (message "%s %s %s %s" name docs file test)
 ;;     ))
 
-;; used in ut-toggle
-(defun ut-prod-file (file &optional marker)
+;; used in td-toggle
+(defun td-prod-file (file &optional marker)
   "returns a sensible production file name, without path. 
 name-marker.ext -> name.ext if marker is given
 name-test.ext   -> name.ext otherwise"
   (or marker (setq marker "-test"))
   (replace-regexp-in-string marker "" file))
 
-;; used in ut-toggle
-(defun ut-test-filep (file &optional marker) 
+;; used in td-toggle
+(defun td-test-filep (file &optional marker) 
   "returns true if file is of the form name-marker.ext or name-test.ext"
   (or marker (setq marker "-test"))
   (equal (first (split-string file marker)) file))
 
 ;; used in switch-to-buffer-or-find-file
-(defun ut-toggle (&optional file marker)
+(defun td-toggle (&optional file marker)
   "toggles between name-test.ext and name.ext"
   (or file (setq file (buffer-name)))
   (or marker (setq marker "-test"))
-  (if (ut-test-filep file marker)
-      (ut-test-file file marker)
-    (ut-prod-file file marker)))
+  (if (td-test-filep file marker)
+      (td-test-file file marker)
+    (td-prod-file file marker)))
 
 ;;; toggle style when selecting "the other file"
-(defun ut-find-file-pair (file &optional marker)
+(defun td-find-file-pair (file &optional marker)
   "opens two files"
   (interactive "FFind files:")
   (or marker (setq marker "-test"))
   (split-window)
-  (ut-other-file file marker)
+  (td-other-file file marker)
   (find-file-other-window file))
 
-(defun ut-other-file (&optional file marker)
+(defun td-other-file (&optional file marker)
   "opens name-marker.ext for file name.ext"
   (interactive)
   (or file (setq file (buffer-name)))
   (or marker (setq marker "-test"))
-  (let ((target (ut-toggle file)))
+  (let ((target (td-toggle file)))
     (if (get-buffer target) 
 	(switch-to-buffer target) 
       (find-file target))))
 
-(defun ut-other-file-other-window ()
-  "ut-other-file in other-window"
+(defun td-other-file-other-window ()
+  "td-other-file in other-window"
   (interactive)
   (split-window)
-  (ut-other-file))
+  (td-other-file))
 
-(global-set-key [f7] 'ut-other-file)
+(global-set-key [f7] 'td-other-file)
 
 
