2a0af1f914ee14709b061782a3c97a72540b8fdc
Karsten Lang <karstenlang@gmail.com>
2009-04-09 13:47:58 +0200

    shell related modes in one place

--- 50-sh-and-term.el
+++
@@ -1,6 +0,0 @@
-(if (include 'shell)
-    ;; Have some reasonable indentation
-    (setq-default sh-indentation 2))
-
-;;; Make some radical improvements on interfacing to "external" programs
-(include 'term)
