e37e275e8f6c42fb895c47a7f11e0939142d4ab1
Karsten Lang <karstenlang@gmail.com>
2009-04-09 13:40:39 +0200

    moving key definitions

--- 99-key-bindings.el
+++ 99-key-bindings.el
@@ -1,23 +1,42 @@
 ;;; Key bindings
-(global-set-key [C-S-tab] 'previous-buffer)
-(global-set-key [C-S-iso-lefttab] 'previous-buffer)
-(global-set-key [C-tab] 'next-buffer)
-(global-set-key [C-S-f6] 'previous-buffer)
-(global-set-key [C-f6] 'next-buffer)
-
-(global-set-key [C-M-up] '(lambda () (interactive) (shrink-window -1)) )
-(global-set-key [C-M-down] '(lambda () (interactive) (shrink-window 1)) )
-;; (global-set-key [C-M-left] '(lambda () (interactive) (shrink-window -1 1)) )
-;; (global-set-key [C-M-right] '(lambda () (interactive) (shrink-window 1 1)) )
-
 (global-set-key [f4] 'speedbar-get-focus)
 (define-key global-map [(meta return)] 'hippie-expand)
 ;; remove try-expand-line from default value
 (setq hippie-expand-try-functions-list (remove 'try-expand-line hippie-expand-try-functions-list))
 ;; get out of recursive editing mode [[[(fundamental)]]]
 (global-set-key [f2] 'top-level)
+
+;;; mess aroud with backspace/insert/delete
+(global-unset-key [insert])
+(global-set-key  [delete] 'delete-char)
+
+;;; some easy point<->register swappin
+(cond ((string-match "XEmacs" (emacs-version))
+       (global-set-key '(control >) 'point-to-register)
+       (global-set-key '(control <) 'register-to-point))
+      (t
+       (global-set-key [?\C->] 'point-to-register)
+       (global-set-key [?\C-<] 'register-to-point)))
+
+;;; Keys handling buffers/windows/frames
+(global-set-key [C-S-tab] 'previous-buffer)
+(global-set-key [C-S-iso-lefttab] 'previous-buffer)
+(global-set-key [C-tab] 'next-buffer)
+(global-set-key [C-S-f6] 'previous-buffer)
+(global-set-key [C-f6] 'next-buffer)
+(global-set-key [C-M-up] '(lambda () (interactive) (shrink-window -1)) )
+(global-set-key [C-M-down] '(lambda () (interactive) (shrink-window 1)) )
+;; (global-set-key [C-M-left] '(lambda () (interactive) (shrink-window -1 1)) )
+;; (global-set-key [C-M-right] '(lambda () (interactive) (shrink-window 1 1)) )
 (global-set-key [f3] 'other-frame)
 (global-set-key [C-f3] 'other-window)
+(global-set-key "\C-xo" 'other-window)
+(global-set-key "\C-x1" 'delete-other-windows)
+(global-set-key "\C-xk" 'kill-buffer)
+(global-set-key "\C-c\C-e" 'eval-current-buffer)
+(if (functionp 'indent-buffer) (global-set-key "\C-ci" 'indent-buffer))
+(global-set-key "\C-xi" 'insert-file)
+
 (global-set-key [f9] '(lambda () (interactive) (save-buffer) (server-edit)) )
 (global-set-key [C-f11] 'dos2unix)
 (global-set-key [(f8)] 'narrow-to-function)
@@ -32,9 +51,16 @@
 ; run buffer as php/perl/sh script
 (global-set-key [f10] 'execute-buffer-script)
 
+;;; just some easy shortcuts
+(global-set-key "\C-cg" 'goto-line)
+(global-set-key "\C-c\C-a" 'auto-fill-mode)
+(global-set-key "\C-c\C-r" 'toggle-read-only)
+(global-set-key "\C-c\C-c" 'comment-region)
+(global-set-key "\C-s" 'isearch-forward)
+(global-set-key "\M-g" 'goto-line)
+
 ; toggles linenum+highlight mode
 (global-set-key [C-f12] '(lambda () (interactive) (highlight-current-line-minor-mode nil) (if (include 'linum) (linum))))
-(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 
