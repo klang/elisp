1f807107b10cd0b3ad999a24e4eb54ab71d7250a
Karsten Lang <karstenlang@gmail.com>
2009-04-29 22:26:27 +0200

    some variables are already set but when cygwin is installed somewhere else, we have a backup plan

--- 50-ssh.el
+++ 50-ssh.el
@@ -3,9 +3,11 @@
   ;; to make tramp work with plink, putty and pageant
   (setq exec-path (cons "~/bin" exec-path))
   (setq tramp-default-method "plink")
-  (setq exec-path (cons "c:/cygwin/bin/" exec-path))
   ;; cygwin replaces bash
   (require 'setup-cygwin)
+  ;; for emacs running on citrix, cygwin is installed somewhere else
+  ;;(setenv "PATH" (concat "f:/cygwin/bin;" (getenv "PATH")))
+  ;;(setq exec-path (cons "f:/cygwin/bin/" exec-path))
   (load "cygwin-tab-complete"))
  ((eq system-type 'gnu/linux)
   (setq tramp-default-method "ssh")
