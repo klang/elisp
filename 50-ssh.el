bcd6b670d759a2f92afdc52ef526fd7efc472e84
Karsten Lang <karstenlang@gmail.com>
2009-05-07 08:58:20 +0200

    adding cygwin/bin to system path from within emacs

--- 50-ssh.el
+++ 50-ssh.el
@@ -1,14 +1,16 @@
 (cond
  ((eq system-type 'windows-nt)
   ;; to make tramp work with plink, putty and pageant
+  ;; for emacs running on citrix, cygwin is installed somewhere else
+  (setenv "PATH" (concat "c:/cygwin/bin;" (getenv "PATH")))
+  (setq exec-path (cons "c:/cygwin/bin/" exec-path))
   (setq exec-path (cons "~/bin" exec-path))
   (setq tramp-default-method "plink")
   ;; cygwin replaces bash
   (require 'setup-cygwin)
-  ;; for emacs running on citrix, cygwin is installed somewhere else
-  ;;(setenv "PATH" (concat "f:/cygwin/bin;" (getenv "PATH")))
-  ;;(setq exec-path (cons "f:/cygwin/bin/" exec-path))
+
   (load "cygwin-tab-complete"))
  ((eq system-type 'gnu/linux)
   (setq tramp-default-method "ssh")
   (setq exec-path (cons "~/bin/" exec-path))))
+
