79274a0662e6cc2c234348d1bfe5541db8456456
Karsten Lang <karstenlang@gmail.com>
2009-05-25 08:59:27 +0200

    clojure-slime-config only works if swank-clojure-extra-classpaths is set

--- 50-clojure-mode.el
+++ 50-clojure-mode.el
@@ -1,28 +1,28 @@
-(setenv "JAVA_HOME" "c:/jdk1.6.0_11")
-(setenv "ANT_HOME" "~/tools/apache-ant-1.7.1")
-(setenv "PATH" 
-	(concat (getenv "ANT_HOME") "/bin;" 
-		(getenv "JAVA_HOME") "/bin;" 
-		(getenv "PATH")))
+(when (eq system-type 'windows-nt)
+  (setenv "JAVA_HOME" "c:/jdk1.6.0_11")
+  (setenv "ANT_HOME" "~/tools/apache-ant-1.7.1")
+  (setenv "PATH" 
+	  (concat (getenv "ANT_HOME") "/bin;" 
+		  (getenv "JAVA_HOME") "/bin;" 
+		  (getenv "PATH"))))
 
 (setq clojure-src-root "~/lisp/clj")
 
-; this does make origin/master work, on linux and mac
- (setq swank-clojure-extra-classpaths
-       (cons (expand-file-name (concat clojure-src-root "/swank-clojure/"))
- 	    (when (file-directory-p "~/.clojure")
- 	      (directory-files "~/.clojure" t ".jar$"))))
+;; git clone git://github.com/technomancy/clojure-mode.git
+(setq swank-clojure-extra-classpaths
+      (cons (concat clojure-src-root "/swank-clojure/")
+    	    (when (file-directory-p "~/.clojure")
+    	      (directory-files "~/.clojure" t ".jar$"))))
 
-
-
-(add-to-list 'load-path "~/lisp/clj/clojure-mode")
+(add-to-list 'load-path "~/lisp/clj/technomancy_clojure-mode")
+;(add-to-list 'load-path "~/lisp/clj/jochu_clojure-mode")
 (require 'clojure-mode)
 
 ;; M-x clojure-install
 ;; we let clojure-mode get all the source clojure, clojure-contrib,
 ;; swank-clojure and slime AND do the compile for us.
 ;;
-;; as clojure and swank-clojure does not play nice at the moment
+;; if clojure and swank-clojure does not play nice
 ;; rolling back to origin/1.0 will make things work again.
 ;; 
 ;; > cd ~/lisp/clj/clojure
