7bd3948a1a13f304958f18ec579439e590040bdb
Karsten Lang <karsten@lang.dk>
2010-04-20 12:15:42 +0200

    custom

---
+++ .emacs-custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(org-todo-keyword-faces (quote (("STOP" :foreground "red" :weight bold :underline nil) ("TODO" :foreground "green" :weight bold :underline nil) ("DONE" :foreground "blue" :weight bold :underline nil) ("WAIT" :foreground "orange" :weight bold :underline nil))))
+ '(org-todo-keywords (quote ((sequence "TODO" "DONE" "WAIT" "STOP")))))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
